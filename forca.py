a43b33f6c0458865851c9af316f0f18cdcf6db9c
Izabel Vidal <izabelvidal99@gmail.com>
2021-12-18 16:14:05 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,23 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "BANANA"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra?").strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}!".format(letra, index))
+            index += 1
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
